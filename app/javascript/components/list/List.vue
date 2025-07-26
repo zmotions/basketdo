96e81eb15ea9f7439365d108ce0c3191c9cf2773
Zafer Dogan <info@z-motions.ch>
2025-07-27 01:41:14 +0200

    feat: add list wrapper components
    - for working and displaying with collections
    - name might change later

---
+++ app/javascript/components/list/List.vue
@@ -0,0 +1,37 @@
+<script setup>
+
+</script>
+
+<template>
+<div class="container-fluid">
+  <div class="row">
+    <div v-if="$slots.sidebar" class="col-3">
+      <slot name="sidebar"></slot>
+    </div>
+    <div :class="{'col': !$slots.sidebar, 'col-9': $slots.sidebar}">
+      <div class="row row-cols-1">
+        <div class="col">
+          <slot name="filter"></slot>
+        </div>
+        <div class="col mt-2">
+          <slot name="actions"></slot>
+        </div>
+      </div>
+      <div class="row mt-2">
+        <div class="col">
+          <slot name="items"></slot>
+        </div>
+      </div>
+      <div class="row mt-2">
+        <div class="col">
+          <slot name="pagination"></slot>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
