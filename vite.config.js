c6a979af1edbf6a70f955945c960990f01ad0489
Zafer Dogan <info@z-motions.ch>
2025-09-13 20:09:39 +0200

    feat: make vite ruby env work forth backend / frontend
    - need to run config() in vite.config.js
    - this way, env variable can be set once in .env files

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,16 @@
 import { defineConfig } from 'vite';
-import RubyPlugin from 'vite-plugin-ruby';
-import FullReload from "vite-plugin-full-reload";
+import ViteRails from 'vite-plugin-rails';
 import Vue from '@vitejs/plugin-vue'
 import ViteYaml from '@modyfi/vite-plugin-yaml';
 import AutoImport from 'unplugin-auto-import/vite'
 import * as path from "path";
+import { config } from 'dotenv';
+
+// the vite-plugin-ruby, part of vite-plugin-rails, exposed env variables that can be set for
+// the backend and frontend, but both in backend and frontend dotenv needs to be installed to read
+// from the .env - files and loaded accordingly, here with config(), does so automatically on the rails said
+// when the gem is loaded
+config();
 
 export default defineConfig({
   test: {
@@ -12,6 +18,9 @@ export default defineConfig({
     environment: "jsdom",
   },
   plugins: [
+    ViteYaml(),
+    Vue(),
+    ViteRails(),
     AutoImport({
       // targets to transform
       include: [
@@ -26,26 +35,8 @@ export default defineConfig({
         'vue-i18n'
       ]
     }),
-    RubyPlugin(),
-    Vue(),
-    ViteYaml(),
-    RubyPlugin(),
-    FullReload(["config/routes.rb", "app/views/**/*"], { delay: 200 }),
   ],
-  root: "./app/assets",
-  build: {
-    manifest: true,
-    rollupOptions: {
-      input: "/app/javascript/entrypoints/application.js"
-    }
-  },
   define: { "process.env": {} },
-  server: {
-    hmr: {
-      host: 'localhost',
-      protocol: 'ws'
-    }
-  },
   resolve: {
     alias: {
       '~bootstrap': path.resolve(__dirname, 'node_modules/bootstrap'),
